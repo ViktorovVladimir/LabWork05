9f49a3c8d03b26015ae9b5b2a61d7d25fb4e43d9
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-27 14:34:32 +0300

    - add ex.3 test task
    - добавил программу для выполнения контрольного упражнения №3 "Обработка данных массива"

---
+++ Lab06/Starter/TestTask/TestTask/Program.cs
@@ -0,0 +1,131 @@
+﻿
+
+namespace TestTaskNameSpace
+{
+    using System;
+
+    //--.
+    public class Test
+    {
+        //--.
+        static void Output(int[,] result)
+        {
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //--.
+        static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
+            result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
+            result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
+            result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            return result;
+        }
+
+        //--.
+        static int[,] Multiply_cicle(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+
+            //--.
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+
+            return result;
+        }
+
+        //--.
+        static void Input(int[,] dst)
+        {
+            //--.
+            for (int r = 0; r < dst.GetLength(0); r++)
+            {
+                for (int c = 0; c < dst.GetLength(1); c++)
+                {
+                    //--.
+                    Console.Write("Enter value for [{0},{1}] : ", r, c);
+                    //--.
+                    dst[r, c] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //--.
+            Console.WriteLine();
+        }
+
+
+        //--.
+        public static void Main()
+        {
+            //--.
+            int[,] a = new int[2, 2];
+            /*
+            a[0, 0] = 1;
+            a[0, 1] = 2;
+            a[1, 0] = 3;
+            a[1, 1] = 4;
+            */
+            Input(a);
+
+
+            //--.
+            int[,] b = new int[2, 2];
+            /*
+            b[0, 0] = 5;
+            b[0, 1] = 6;
+            b[1, 0] = 7;
+            b[1, 1] = 8;
+            */
+            Input(b);
+
+
+            //--.
+            int[,] result = Multiply(a, b);
+            /*
+            //--.
+            int[,] result = new int[2, 2];
+            result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
+            result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
+            result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
+            result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            */
+
+            //--. 
+            Console.WriteLine(result[0, 0]);
+            Console.WriteLine(result[0, 1]);
+            Console.WriteLine(result[1, 0]);
+            Console.WriteLine(result[1, 1]);
+
+            //--.
+            Console.Write("\n");
+
+            //--.
+            Output(result);
+
+            //--.
+            Console.Write("\n");
+
+            //--.
+            //--.
+            result = Multiply_cicle(a, b);
+            //--.
+            Output(result);
+
+
+        }
+    }
+}
