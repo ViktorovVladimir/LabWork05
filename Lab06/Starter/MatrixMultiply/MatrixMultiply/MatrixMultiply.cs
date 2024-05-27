4923645c8b5257d34e0581a38273a8cb5a68cf7a
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-27 12:52:52 +0300

    - ex.2 massive result use cicle for
    - рассчитал значение массива result, используя цикл for

--- Lab06/Starter/MatrixMultiply/MatrixMultiply/MatrixMultiply.cs
+++ Lab06/Starter/MatrixMultiply/MatrixMultiply/MatrixMultiply.cs
@@ -4,19 +4,19 @@
 
     //--.
     public class Test
-    {
+    {   
         //--.
         static void Output(int[,] result)
-        {
+        {   
             for(int r = 0; r<result.GetLength(0); r++)
-            {
+            {   
                 for(int c = 0; c<result.GetLength(1); c++)
                 {
                     Console.Write("{0} ", result[r, c]);
                 }
                 Console.WriteLine();
-            }
-        }
+            }   
+        }   
 
         //--.
         static int[,] Multiply(int[,] a, int [,] b)
@@ -28,6 +28,24 @@
             result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
             return result;
         }
+
+        //--.
+        static int[,] Multiply_cicle(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            
+            //--.
+            for(int r=0; r<result.GetLength(0); r++)
+            {
+                for(int c=0; c<result.GetLength(1); c++)
+                {
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+                        
+            return result;
+        }
+
 
         //--.
         public static void Main()
@@ -68,6 +86,17 @@
 
             //--.
             Output(result);
+
+            //--.
+            Console.Write("\n");
+
+            //--.
+            //--.
+            result = Multiply_cicle(a, b);
+            //--.
+            Output(result);
+
+
         }
     }
 }
